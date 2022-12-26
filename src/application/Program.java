64250c83eb05ed931335961a09435d5e1d92b809
Jefferson Richards <jefferson.richards@outlook.com>
2022-12-26 15:32:48 -0300

    Adiconando comentarios

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,18 +15,21 @@ public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		List<Produtos> list = new ArrayList<>();
-
+		
+	   //    C:\\Users\\jeffe\OneDrive\\Área de Trabalho\\Diretorio\\source.csv
+		
 		System.out.println("Digite o caminho do arquivo");
-
-		String path = sc.nextLine();
+        String path = sc.nextLine();
+        
 		File sourceFile = new File(path);
+		
 		try (BufferedReader file = new BufferedReader(new FileReader(path))) {
 
 			String line = file.readLine();
 			while (line != null) {
 				String tratamento = line;
 				String[] vect = tratamento.split(",");
-				line = file.readLine();
+				line = file.readLine(); // comando para passar para a proxima linha
 				list.add(new Produtos(vect[0], Double.valueOf(vect[1]), Integer.valueOf(vect[2])));
 			}
 
